01597f32f3a2d592d513cd55a6af255e5864c154
Kateryna Pohorila <katpog2001@gmail.com>
2025-09-09 23:22:47 +0300

    add cards with random countries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { CelebratingService } from '../celebrating.service';
+import { Country } from '../core/models/country';
+import { Holiday } from '../core/models/holiday';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-home',
+  imports: [AsyncPipe, MatCardModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  countriesList$!: Observable<Country[]>;
+  randomHolidays$!: Observable<Holiday[]>;
+  countriesByCode$!: Observable<Map<string, string>>;
+
+  private celebratingService = inject(CelebratingService);
+
+  ngOnInit() {
+    this.countriesList$ = this.celebratingService.getAllCountries();
+    this.countriesByCode$ = this.countriesList$.pipe(
+      map((list: Country[]) => new Map(list.map((c: Country) => [c.countryCode, c.name]))),
+    );
+    this.randomHolidays$ = this.celebratingService.getRandomHolidays();
+  }
+}
